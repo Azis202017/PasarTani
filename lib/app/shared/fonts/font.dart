c33bb6a61d8fe558a3fcf438f18c039f19e95b35
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-13 20:05:35 +0700

    [Font] added font color

---
+++ lib/app/shared/fonts/font.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle titleLogin =
+    GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w600);
+TextStyle subtitleLogin =
+    GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400);
